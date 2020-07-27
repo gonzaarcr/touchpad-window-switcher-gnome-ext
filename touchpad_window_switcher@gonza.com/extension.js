a5c0b78147ec4f4e11ba7c952c6f4fae324bc66a
gonza <gonza.ar.cr@gmail.com>
2020-07-27 11:17:40 -0300

    Added two gestures: show overview and show desktop

--- touchpad_window_switcher@gonza.com/extension.js
+++ touchpad_window_switcher@gonza.com/extension.js
@@ -1,19 +1,23 @@
 
 const Clutter = imports.gi.Clutter;
-const Lang = imports.lang;
+const Gio = imports.gi.Gio;
 const Meta = imports.gi.Meta;
+const St = imports.gi.St;
+const Lang = imports.lang;
 const Signals = imports.signals;
+
+const Main = imports.ui.main;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
-const MySwitcherPopup = Me.imports.mySwitcherPopup;
 const MyAltTab = Me.imports.myAltTab;
 
 let gestureHandler = null;
 
 let popup = null;
 
-const MOTION_THRESHOLD = 400;
+const MOTION_THRESHOLD = 200;
+
 
 const TouchpadGestureAction = new Lang.Class({
 	Name: 'TouchpadGestureAction',
@@ -38,11 +42,12 @@ const TouchpadGestureAction = new Lang.Class({
 
 		this.DIRECTION_LOOKUP = {
 			0: Meta.MotionDirection.RIGHT,
-			1: Meta.MotionDirection.UP,
+			1: Meta.MotionDirection.DOWN,
 			2: Meta.MotionDirection.LEFT,
-			3: Meta.MotionDirection.DOWN
+			3: Meta.MotionDirection.UP
 		};
 		this._actionCallbackID = this.connect('activated', Lang.bind(this, this._doAction));
+		this._lastVertical = 0;
 	},
 
 	_handleEvent: function(actor, event) {
@@ -62,75 +67,165 @@ const TouchpadGestureAction = new Lang.Class({
 		let magnitude = Math.sqrt(Math.pow(this._dy, 2) + Math.pow(this._dx, 2));
 
 		let rounded_direction = Math.round(Math.atan2(this._dy, this._dx) / Math.PI * 2);
-		if (rounded_direction == -1) {
-			rounded_direction = 3;
-		} else if (rounded_direction == -2) {
-			rounded_direction = 2;
-		}
+		if (rounded_direction < 0)
+			rounded_direction += 4;
+
 		let dir = this.DIRECTION_LOOKUP[rounded_direction];
 
-		// global.log("_handleEvent");
 		switch (event.get_gesture_phase()) {
 			case Clutter.TouchpadGesturePhase.BEGIN:
-				return this._gestureStarted();
+				return this._gestureUpdate(dir, magnitude);
 
 			case Clutter.TouchpadGesturePhase.UPDATE:
 				return this._gestureUpdate(dir, magnitude);
 
-			default: //CANCEL or END
+			default: // CANCEL or END
 				return this._gestureEnd();
 		}
 
 		return Clutter.EVENT_STOP;
 	},
 
-	_gestureStarted: function() {
-		this.emit('activated', 'open-switcher');
-
-		return Clutter.EVENT_STOP;
-	},
-
 	_doAction: function(sender, action) {
 		switch (action) {
-			case 'open-switcher':
-				global.log("open-switcher");
-
-				popup = new MyAltTab.WindowSwitcherPopup();
-				if (!popup.show(false, '', 0)) {
-					tabPopup.destroy();
+			case 'move-right':
+				if (popup == null) {
+					this._createSwitcher();
+				} else {
+					popup._select(popup._next());
 				}
 				break;
-			case 'move-right':
-				popup._select(popup._next());
-				this._dx = 0;
-				this._dy = 0;
-				break;
 			case 'move-left':
-				popup._select(popup._previous());
+				if (popup == null) {
+					this._createSwitcher();
+				} else {
+					popup._select(popup._previous());
+				}
 				this._dx = 0;
 				this._dy = 0;
 				break;
 			case 'close-switcher':
-				popup._finish(global.display.get_current_time_roundtrip());
-				popup = null;
-				this._dx = 0;
-				this._dy = 0;
+				if (popup != null) {
+					popup._finish(global.display.get_current_time_roundtrip());
+					popup = null;
+				}
+				break;
+			case 'show-desktop':
+				this._showDesktop();
+				break;
+			case 'unshow-desktop':
+				// gsettings set org.gnome.desktop.interface enable-animations true
+				this._unshowDesktop();
 				break;
 			default:
 				break;
 		}
 	},
 
-	_gestureUpdate: function(dir, motion) {
-		if (dir == Meta.MotionDirection.RIGHT && motion > MOTION_THRESHOLD) {
-			this.emit('activated', 'move-right');
-			return Clutter.EVENT_STOP;
-		} else if (dir == Meta.MotionDirection.LEFT && motion > MOTION_THRESHOLD) {
-			this.emit('activated', 'move-left');
-			return Clutter.EVENT_STOP;
-		} else {
-			return Clutter.EVENT_PROPAGATE;
+	_createSwitcher: function() {
+		popup = new MyAltTab.WindowSwitcherPopup();
+		if (!popup.show(false, '', 0)) {
+			tabPopup.destroy();
 		}
+	},
+
+	_gestureUpdate: function(dir, motion) {
+		if (motion < MOTION_THRESHOLD)
+			return Clutter.EVENT_PROPAGATE;
+
+		let ret = Clutter.EVENT_PROPAGATE;
+
+		if (dir == Meta.MotionDirection.RIGHT) {
+			if (!Main.overview.visible) {
+				this.emit('activated', 'move-right');
+			} else {
+				Main.overview.viewSelector._workspacesDisplay._getPrimaryView().navigate_focus(null, St.DirectionType.TAB_FORWARD, false);
+			}
+			ret = Clutter.EVENT_STOP;
+		} else if (dir == Meta.MotionDirection.LEFT) {
+			if (!Main.overview.visible) {
+				this.emit('activated', 'move-left');
+			} else {
+				Main.overview.viewSelector._workspacesDisplay._getPrimaryView().navigate_focus(null, St.DirectionType.TAB_BACKWARD, false);
+			}
+			ret = Clutter.EVENT_STOP;
+		} else if (dir == Meta.MotionDirection.UP) {
+			global.log(global.get_current_time() - this._lastVertical);
+			if (popup == null && (global.get_current_time() - this._lastVertical) > 1000) {
+				this._lastVertical = global.get_current_time();
+				if (this._canUnshowDesktop()) {
+					this.emit('activated', 'unshow-desktop');
+					ret = Clutter.EVENT_STOP;
+				} else if (!Main.overview.visible) {
+					Main.overview.show();
+					ret = Clutter.EVENT_STOP;
+				}
+			}
+		} else if (dir == Meta.MotionDirection.DOWN) {
+			global.log(global.get_current_time() - this._lastVertical);
+			if (popup == null && (global.get_current_time() - this._lastVertical) > 1000) {
+				this._lastVertical = global.get_current_time();
+				if (Main.overview.visible) {
+					Main.overview.hide();
+					ret = Clutter.EVENT_STOP;
+				} else {
+					this.emit('activated', 'show-desktop');
+					ret = Clutter.EVENT_STOP;
+				}
+			}
+		}
+		if (ret === Clutter.EVENT_STOP)
+			this._dx = 0;
+			this._dy = 0;
+
+		return ret;
+	},
+
+	_canUnshowDesktop: function() {
+		return !Main.overview.visible && this.toMaximize.length > 0 && this._isShowingDesktop();
+	},
+
+	_isShowingDesktop: function() {
+		let windows = global.workspace_manager.get_active_workspace().list_windows();
+		global.log(windows.map(x => x.minimized).join(', '));
+		return windows.every(x => x.minimized);
+	},
+
+	_showDesktop: function() {
+		this._disableAnimations();
+		// TODO para distintos workspaces
+		this.toMaximize = [];
+		let windows = global.workspace_manager.get_active_workspace().list_windows();
+		for (let i = 0; i < windows.length; i++) {
+			if (!windows[i].minimized) {
+				this.toMaximize.push(windows[i]);
+				windows[i].minimize();
+			}
+		}
+	},
+
+	_unshowDesktop: function() {
+		this._disableAnimations();
+		let windows = global.workspace_manager.get_active_workspace().list_windows();
+		for (let i = 0; i < windows.length; i++) {
+			if (this.toMaximize.indexOf(windows[i]) !== -1)
+				windows[i].unminimize();
+		}
+		this.toMaximize = [];
+		this._restoreAnimations();
+	},
+
+	_disableAnimations: function() {
+		this._settings = new Gio.Settings({ schema_id: 'org.gnome.desktop.interface' });
+		this._defaultAnimation = this._settings.get_boolean('enable-animations');
+		this._settings.set_boolean('enable-animations', false);
+	},
+
+	_restoreAnimations: function() {
+		if (this._defaultAnimation)
+			this._settings.set_boolean('enable-animations', true);
+		this._settings = null;
+		this._defaultAnimation = null;
 	},
 
 	_gestureEnd: function() {
