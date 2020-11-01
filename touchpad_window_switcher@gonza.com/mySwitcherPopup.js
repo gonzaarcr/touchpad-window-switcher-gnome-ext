37411cfd9c4e1b79c033cf63012cbfb02d015727
Gonzalo <gonza.ar.cr@gmail.com>
2020-10-31 23:29:27 -0300

    Update mySwitcherPopup.js
    Support for 3.38

--- touchpad_window_switcher@gonza.com/mySwitcherPopup.js
+++ touchpad_window_switcher@gonza.com/mySwitcherPopup.js
@@ -318,7 +318,7 @@ var SwitcherPopup = GObject.registerClass({
             this.ease({
                 opacity: 0,
                 duration: POPUP_FADE_OUT_TIME,
-                mode: Clutter.Animation.EASE_OUT_QUAD,
+                mode: Clutter.Animation ? Clutter.Animation.EASE_OUT_QUAD : Clutter.AnimationMode.EASE_OUT_QUAD , // 3.38
                 onComplete: () => this.destroy(),
             });
         } else {
